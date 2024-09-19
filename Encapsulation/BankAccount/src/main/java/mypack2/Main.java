975879113309739f137bf568e6e177eb30a50f25
Lumbini Chathurani <lumbinichathurani@gmail.com>
2024-09-19 19:15:48 +0530

    Update BankAccount and Main files, and modify project configuration

--- Encapsulation/BankAccount/src/main/java/mypack2/Main.java
+++ Encapsulation/BankAccount/src/main/java/mypack2/Main.java
@@ -1,19 +1,16 @@
 package mypack2;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BankAccount account = new BankAccount();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        account.setAccountNumber("123");
+        account.setBalance(1500);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String accountNumber = account.getAccountNumber();
+        double balance = account.getBalance();
+
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
     }
 }
