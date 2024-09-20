a2e4f9bd52620be2a016dcb6f321c2770377b812
Lumbini Chathurani <lumbinichathurani@gmail.com>
2024-09-20 12:20:08 +0530

    Add rectangle class

--- Encapsulation/Employee/src/main/java/mypack2/Main.java
+++ Encapsulation/Employee/src/main/java/mypack2/Main.java
@@ -2,7 +2,17 @@ package mypack2;
 
 public class Main {
     public static void main(String[] args) {
+Employee e1 = new Employee();
+e1.setEmployee_id(123);
+e1.setEmployee_name("Lumbini");
+e1.setEmployee_salary(50000.0);
 
+int employeeId= e1.getEmployee_id();
+String employeeName=e1.getEmployee_name();
+double employeeSalary=e1.getEmployee_salary();
 
+        System.out.println("Employee ID: "+employeeId);
+        System.out.println("Employee Name: "+employeeName);
+        System.out.println("Employee Salary: "+employeeSalary);
     }
 }
