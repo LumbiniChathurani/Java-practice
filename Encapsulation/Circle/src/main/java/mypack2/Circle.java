824bf0ab016eec661972608a96bd1c32484e7a0c
Lumbini Chathurani <lumbinichathurani@gmail.com>
2024-09-20 13:24:39 +0530

    Add codes to Circle class and Main class

--- Encapsulation/Circle/src/main/java/mypack2/Circle.java
+++ Encapsulation/Circle/src/main/java/mypack2/Circle.java
@@ -2,5 +2,16 @@ package mypack2;
 
 public class Circle {
     private double radius;
-
+public double getRadius(){
+    return radius;
+}
+public void setRadius(double radius){
+    this.radius=radius;
+}
+public double getArea(){
+    return Math.PI*radius*radius;
+}
+public double getPerimeter(){
+    return 2*Math.PI*radius;
+}
 }
--- Encapsulation/Circle/src/main/java/mypack2/Main.java
+++ Encapsulation/Circle/src/main/java/mypack2/Main.java
@@ -2,6 +2,13 @@ package mypack2;
 
 public class Main {
     public static void main(String[] args) {
-
+Circle c1 = new Circle();
+c1.setRadius(35);
+double radius = c1.getRadius();
+double area = c1.getArea();
+double perimeter = c1.getPerimeter();
+        System.out.println("Radius: "+radius);
+        System.out.println("Area: "+area);
+        System.out.println("Perimeter: "+perimeter);
     }
 }
